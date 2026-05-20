e531e1c0c07db35df99e55d67976d3ba9d6d43c6
Muhammed Nihal P N <muhammednihalpn@gmail.com>
2026-05-20 17:00:03 +0530

    yml

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,6 +6,7 @@ import xgboost as xgb
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
